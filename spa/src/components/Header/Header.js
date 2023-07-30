2868a2037bee9cbab41b337f873ecfe37497cfcf
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-07-30 15:48:26 +0300

    Update header js

--- spa/src/components/Header/Header.js
+++ spa/src/components/Header/Header.js
@@ -1,32 +1,34 @@
+import Menu from "../Mobile/Menu/Menu";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
-
+import { AiOutlineUser } from "react-icons/ai";
 
 const Header = () => {
   return (
     <header className={styles.sticky}>
       <nav className={styles.container}>
         <Link className={styles.logo} to="/">
-          <img src='image006.jpg'  alt="logo"></img>
+          <img src="image006.jpg" alt="logo"></img>
         </Link>
-        <ul className={styles.box}>
-          <li>
-            <Link className={styles["link"]} to="/">
-              Shorts
-            </Link>
-          </li>
-          <li>
-            <Link className={styles["link"]} to="/shoes">
-              Shoes
-            </Link>
-          </li>
-          <li>
-            <Link className={styles["link"]} to="/shirts">
-              T-shirts
-            </Link>
-          </li>
-         
-        </ul>
+        <div className={styles["nav-mobile"]}>
+          <Menu />
+        </div>
+        <div className={styles.icon}>
+          <AiOutlineUser size={25} color="black" />
+        </div>
+        <div className={styles.ul_container}>
+          <ul className={styles.box}>
+            <li>
+              <Link to="/">Shorts</Link>
+            </li>
+            <li>
+              <Link to="/shoes">Shoes</Link>
+            </li>
+            <li>
+              <Link to="/shirts">T-shirts</Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
