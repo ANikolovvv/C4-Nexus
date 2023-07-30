3c04f1c7cf37d45ce2b10c11c52f8a998b6ad351
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-07-30 15:50:12 +0300

    Update Button.js

--- spa/src/components/UI/Button/Button.js
+++ spa/src/components/UI/Button/Button.js
@@ -1,11 +1,10 @@
-import { useState } from "react";
 import styles from "./Button.module.css";
 
-const Button = ({ onClick ,showMore }) => {
+const Button = ({ onClick, stock }) => {
   return (
     <div className={styles.load_box}>
       <button className={styles.load_btn} onClick={onClick}>
-      {showMore ? 'Load More' : 'Load Less'}
+        {stock.length <= 4 ? "Load More" : "Load Less"}
       </button>
     </div>
   );
