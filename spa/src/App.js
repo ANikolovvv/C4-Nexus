5e28ed7ac61d4ad419241e89b343a3d052535e65
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-07-30 15:46:50 +0300

    Add data provider to App

--- spa/src/App.js
+++ spa/src/App.js
@@ -7,20 +7,23 @@ import Footer from "./components/Footer/Footer";
 import Shoes from "./components/Pages/Shoes/Shoes";
 import Shirts from "./components/Pages/Shirts/Shirts";
 import Shorts from "./components/Pages/Shorts/Shorts";
+import DataProvider from "./context/dataContext";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <main className="main">
-        <Routes>
-          <Route path="/" element={<Shorts />}></Route>
-          <Route path="/shoes" element={<Shoes />}></Route>
-          <Route path="/shirts" element={<Shirts />}></Route>
-        </Routes>
-        <Footer />
-      </main>
-    </div>
+    <DataProvider>
+      <div className="App">
+        <Header />
+        <main className="main">
+          <Routes>
+            <Route path="/" element={<Shorts />}></Route>
+            <Route path="/shoes" element={<Shoes />}></Route>
+            <Route path="/shirts" element={<Shirts />}></Route>
+          </Routes>
+          <Footer />
+        </main>
+      </div>
+    </DataProvider>
   );
 }
 
