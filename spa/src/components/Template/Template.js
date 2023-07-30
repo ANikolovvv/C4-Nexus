90ddeb52712940b8e235bd5d8edcfbf9e11abc8b
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-07-30 15:50:04 +0300

    Update Template.js

--- spa/src/components/Template/Template.js
+++ spa/src/components/Template/Template.js
@@ -1,18 +1,21 @@
 import styles from "./Template.module.css";
-
 import { useState } from "react";
+import { sortingHandler } from "../../utils/helpers";
 
 import FilterForm from "../FilterForm/FilterForm";
 import Button from "../UI/Button/Button";
-import { sortingHandler } from "../../utils/helpers";
 import Title from "../UI/Title/Title";
+
 import SortGrid from "../SortGrid/SortGrid";
 import Card from "../Card/Card";
+
 
 const Template = ({ data, title, desc }) => {
   const [stock, setStock] = useState(data.slice(0, 4));
   const [showMore, setShowMore] = useState(true);
+  const [sort,setSort] = useState([])
 
+  
   const handleFilter = (e) => {
     let filter = e.length > 0 ? e.slice(0, 4) : stock;
     setStock(filter);
@@ -20,14 +23,15 @@ const Template = ({ data, title, desc }) => {
 
   const handleSearch = (e) => {
     let find = sortingHandler(data, e);
+    setSort(find)
     let top = find.slice(0, 4);
-    console.log(top,'top')
     setStock(top);
   };
 
   const loadHandler = (e) => {
     if (showMore) {
-      setStock(data);
+      let current=sort.length > 0 ? sort : data
+      setStock(current);
     } else {
       setStock(data.slice(0, 4));
     }
@@ -35,21 +39,21 @@ const Template = ({ data, title, desc }) => {
     setShowMore(!showMore);
   };
   return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.header_box}>
-          <FilterForm data={data} onFilter={handleFilter} />
-          <Title category={title} description={desc} />
-          <SortGrid data={data} onSearch={handleSearch} />
-        </div>
-        <div className={styles.cards_box}>
-          {stock.map((x) => (
-            <Card key={x.image} info={x} />
-          ))}
-        </div>
-        <Button onClick={loadHandler} showMore={showMore} />
+    <div className={styles.container}>
+      <div className={styles.header_box}>
+        <FilterForm data={data} onFilter={handleFilter} />
+        <Title data={data} category={title} description={desc} stock={stock} />
+        <SortGrid data={data} onSearch={handleSearch} />
       </div>
-    </>
+      <div className={styles.cards_box}>
+        {stock.map((x) => (
+          <Card key={x.image} info={x} />
+        ))}
+      </div>
+      <div className={styles.btn_box}>
+        <Button onClick={loadHandler}  stock={stock} />
+      </div>
+    </div>
   );
 };
 
