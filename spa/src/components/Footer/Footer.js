4e835347a0807e288e9ed602ef60f1a82621a064
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-07-31 15:37:37 +0300

    Update Footer.js

--- spa/src/components/Footer/Footer.js
+++ spa/src/components/Footer/Footer.js
@@ -1,14 +1,15 @@
+import { Link } from "react-router-dom";
+import styles from "./Footer.module.css";
+import PngsBox from "../UI/Pngs/Pngs";
 
-import { Link } from 'react-router-dom';
-import styles from './Footer.module.css'; 
-
-const Footer = () => {
+const Footer = (css) => {
   return (
     <footer className={styles.footer}>
-       <div className={styles.footer_links}>
+      <div className={styles.footer_links}>
         <Link to="/terms-and-conditions">T&C</Link>
         <Link to="/privacy-policy">Privacy Policy</Link>
         <Link to="/contact-us">Contact Us</Link>
+        <PngsBox css={"png_box_footer"} />
       </div>
       <p>&copy; 2023 All rights reserved.</p>
     </footer>
