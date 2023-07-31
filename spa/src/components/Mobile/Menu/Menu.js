48117a2b9d92df48971f8ad6b5fe7910290b7f8d
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-07-31 15:37:46 +0300

    Update Menu.js

--- spa/src/components/Mobile/Menu/Menu.js
+++ spa/src/components/Mobile/Menu/Menu.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import styles from "./Menu.module.css";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import PngsBox from "../../UI/Pngs/Pngs";
+
 
 const Menu = () => {
   const [toggle, setToggle] = useState(false);
@@ -28,6 +30,12 @@ const Menu = () => {
               </li>
             </ul>
           </div>
+          <div className={styles.info_box}>
+            <Link to="/terms-and-conditions">T&C</Link>
+            <Link to="/privacy-policy">Privacy Policy</Link>
+            <Link to="/contact-us">Contact Us</Link>
+          </div>
+          <PngsBox css={'png_box'}/>
         </div>
       ) : (
         <div>
