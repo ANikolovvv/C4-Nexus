97d675c6e6fda628551cde661327819151705b4a
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-08-01 17:55:21 +0300

    Update helpers.js

--- spa/src/utils/helpers.js
+++ spa/src/utils/helpers.js
@@ -15,10 +15,10 @@ export const sortingHandler = (arrayData, sortArray) => {
           .sort((a, b) => b.name.localeCompare(a.name));
         break;
       case "PriceAscending":
-        currentData = arrayData.slice().sort((a, b) => a.price - b.price);
+        currentData = arrayData.sort((a, b) => a.price - b.price);
         break;
       case "Price descending":
-        currentData = arrayData.slice().sort((a, b) => b.price - a.price);
+        currentData = arrayData.sort((a, b) => b.price - a.price);
         break;
       default:
         currentData = arrayData;
@@ -29,19 +29,16 @@ export const sortingHandler = (arrayData, sortArray) => {
   return currentData;
 };
 
-export const filteredData = (data, color, price) => {
-  let filteredByColor = (item) => {
-    if (!color || color !== item.color) {
-      return false;
-    }
-    return true;
-  };
-  let filteredByPrice = (item) => {
-    if (!price || Number(price) > item.price) {
-      return false;
-    }
-    return true;
-  };
-  let filtered = data.filter(filteredByColor).filter(filteredByPrice);
-  return filtered;
+export const filteredData = (data, colors, prices) => {
+  const filteredProducts = data.filter((product) => {
+    const colorMatch = colors.length === 0 || colors.includes(product.color);
+    const priceMatch =
+      prices.length === 0 ||
+      (prices[0] === "0-25" && product.price <= 25) ||
+      (prices[0] === "25-100" && product.price > 25);
+
+    return colorMatch && priceMatch;
+  });
+
+  return filteredProducts;
 };
