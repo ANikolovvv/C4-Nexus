81e45e567acb7d99fae59e268dbe9d6700481a8b
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-08-01 17:55:18 +0300

    Update dataContext.js

--- spa/src/context/dataContext.js
+++ spa/src/context/dataContext.js
@@ -3,6 +3,8 @@ import data from '../data.json'
 
 export const dataContext = createContext();
 
+
+
 const DataProvider = ({ children }) => {
   return <dataContext.Provider value={data}>{children}</dataContext.Provider>;
 };
