125fdffb676453401625be3800479d0f556cf847
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-08-02 12:50:10 +0300

    Create Label.js

---
+++ spa/src/components/UI/Label/Label.js
@@ -0,0 +1,17 @@
+import styles from "./Label.module.css";
+
+const Label = ({type,array,handleTypeChange}) => {
+  return (
+    <label key={type} htmlFor={type} className={styles.label_box}>
+      {type}
+      <input
+        id={type}
+        type="checkbox"
+        value={type}
+        checked={array.includes(type) || false}
+        onChange={handleTypeChange}
+      />
+    </label>
+  );
+};
+export default Label;
