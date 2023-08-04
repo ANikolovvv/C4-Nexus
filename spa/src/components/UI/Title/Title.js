5c7a70e979affa95e00843be2291af3a8a08cb23
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-08-04 18:03:18 +0300

    Update Title.js

--- spa/src/components/UI/Title/Title.js
+++ spa/src/components/UI/Title/Title.js
@@ -4,7 +4,7 @@ import styles from "./Title.module.css";
 const Title = ({ category, description, data, stock }) => {
   return (
     <div className={styles.head_box}>
-      <h2 className={styles.head}>{category}</h2>
+      <h1 className={styles.head}>{category}</h1>
       <p className={styles.desc_title}>{description}</p>
       <Counter count={stock.length} total={data.length} />
     </div>
